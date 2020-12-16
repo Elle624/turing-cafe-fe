88c35316c1f7abce96966022ad3c89cab4cf5e26
Yiran Li <yiran.li@hotmail.com>
2020-12-16 11:23:59 -0700

    Add removeReservation method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -32,5 +32,21 @@ export const apiCalls = {
           throw new Error('Sorry we are having trouble adding reservations for you, please try again')
         }
       })
+  },
+  removeReservation: (id) => {
+    return fetch(`http://localhost:3001/api/v1/reservations/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Sorry we are having trouble removing reservations for you, please try again')
+        }
+      })
   }
 }
