ef38b363fffe9574d95b88496513f0546c0b5265
Yiran Li <yiran.li@hotmail.com>
2020-12-16 10:34:10 -0700

    Create test for testing rendering

--- src/test/Form.test.js
+++ src/test/Form.test.js
@@ -6,4 +6,17 @@ import Form from '../Component/Form';
 
 describe('Form component', () => {
 
+  it('should render all inputs field upon loading', () => {
+    render(<Form />)
+    const name = screen.getByPlaceholderText('Name');
+    const date = screen.getByPlaceholderText('Date(mm/dd)');
+    const time = screen.getByPlaceholderText('Time');
+    const number = screen.getByPlaceholderText('Number of guests');
+
+    expect(name).toBeInTheDocument();
+    expect(date).toBeInTheDocument();
+    expect(time).toBeInTheDocument();
+    expect(number).toBeInTheDocument();
+  })
+
 })
