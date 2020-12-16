dc2f7ee3d30223d9efbd4534ac5e73ad96fba589
Yiran Li <yiran.li@hotmail.com>
2020-12-16 10:08:47 -0700

    Add inputs

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -4,12 +4,23 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-
+      name: '',
+      date: '',
+      time: '',
+      number: ''
     }
   }
 
   render() {
-
+    return (
+      <form className='reservation-form'>
+        <input name='name' placeholder='Name' />
+        <input name='date' placeholder='Date(mm/dd)' />
+        <input name='time' placeholder='Time' />
+        <input name='number' placeholder='Number of guests' />
+        <input type="submit" value='Make Reservation' />
+      </form>
+    )
   }
 }
 
