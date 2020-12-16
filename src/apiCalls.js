edafdd7aade22572110d64dab0326e1278a024f3
Yiran Li <yiran.li@hotmail.com>
2020-12-16 11:11:21 -0700

    Add post method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,8 +9,28 @@ const getData = (path) => {
     })
 }
 
+
+
 export const apiCalls = {
   getReservations: () => {
     return getData('http://localhost:3001/api/v1/reservations')
+  },
+
+  addReservation: (newReservation) => {
+    return fetch('http://localhost:3001/api/v1/reservations', {
+      method: POST,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newReservation)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Sorry we are having trouble adding reservations for you, please try again')
+        }
+      })
   }
 }
