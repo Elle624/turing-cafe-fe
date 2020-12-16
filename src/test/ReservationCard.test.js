820601d4f3b4a0fa2b3c97a18a45cc14e2b2439d
Yiran Li <yiran.li@hotmail.com>
2020-12-16 10:44:34 -0700

    Create ReservationCard test file and initial import

---
+++ src/test/ReservationCard.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ReservationCard from '../Component/ReservationCard';
+
+describe('ReservationCard component', () => {
+
+})
