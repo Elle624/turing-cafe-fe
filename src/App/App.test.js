c43c52bf9a10a2cf27a9d2030e765a29b73ab9ec
Yiran Li <yiran.li@hotmail.com>
2020-12-16 10:58:34 -0700

    Add test for adding new reservation

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,5 +6,23 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
+  it('should be able to add new reservation to display', () => {
+    render(<App />);
 
+    const nameInput = screen.getByPlaceholderText('Name');
+    const dateInput = screen.getByPlaceholderText('Date(mm/dd)');
+    const timeInput = screen.getByPlaceholderText('Time');
+    const numberInput = screen.getByPlaceholderText('Number of guests');
+    const submitButton = screen.getByRole('button');
+    userEvent.type(nameInput, 'Isabel');
+    userEvent.type(dateInput, '05/27');
+    userEvent.type(timeInput, '1:00');
+    userEvent.type(numberInput, '2');
+    userEvent.click(submitButton);
+
+    expect(screen.getByText('Isabel')).toBeInTheDocument();
+    expect(screen.getByText('05/27')).toBeInTheDocument();
+    expect(screen.getByText('1:00')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+  })
 })
