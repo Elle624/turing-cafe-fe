5c1341785ea0bd99e0271fdcc02441419e76e347
Yiran Li <yiran.li@hotmail.com>
2020-12-16 10:29:33 -0700

    Add Form test file, initial import

---
+++ src/test/Form.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/user-event';
+import Form from '../Component/Form';
+
+describe('Form component', () => {
+
+})
